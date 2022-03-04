ef95b34f9f8fb6d7e0c15c3cbb8ff1c834118c2b
mohamed ibnahmad <ibnahmadmohamed8@gmail.com>
2022-03-04 15:13:37 +0000

    daynamic style using props

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+
+<h1>{{title}}</h1>
+ <Modal  header="sing up for give" text="Grab your loren" theme="sale"/>
+
+   
+
+
+</template>
+
+<script>
+import Modal from './components/Modal'
+export default {
+  name: 'App',
+  components:{Modal},
+  data() {
+    return {
+      title: 'mt vue app'
+    }
+  }
+   
+
+
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom:10px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="backdrop">
+        <div class="modal" :class="{sale: theme==='sale'}">
+            <h1>{{header}}</h1>
+            <p>{{text}}</p>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+
+
+export default ({
+    // name :Modal
+    props:['header','text','theme'],
+   
+    
+})
+</script>
+
+<style scoped>
+
+ 
+.modal{
+    width:400px;
+    padding:20px;
+    margin:100px auto;
+    background-color:white;
+    border-radius:10px;
+}
+.backdrop{
+    top:0;
+    position:fixed;
+    background-color:rgba(0, 0, 0, 0.5);
+    width:100%;
+    height:100%;
+}
+h1 { 
+    color:rgb(96, 236, 96);
+    border:none;
+    padding:0;
+}
+.modal.sale{
+    background-color:crimson;
+    color:white;
+
+}
+.modal.sale h1{
+    color:white;
+}
+
+</style>
